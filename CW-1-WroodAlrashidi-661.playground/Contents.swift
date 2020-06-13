bb69f74b2c527582483f42f29b07b2d504483325
wroodalrashidi <66529914+wroodalrashidi@users.noreply.github.com>
2020-06-13 19:15:34 +0300

    part 1 id done

---
+++ CW-1-WroodAlrashidi-661.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let name = "Wrood"
+var hobby1 = "reading"
+var hobby2 = "drawing"
+var age = 17
+var height = 160
+print(name)
+print(hobby1)
+print(hobby2)
+print(age)
+print(height)
